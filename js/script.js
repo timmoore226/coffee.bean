d95143b2ba474199d0aa8945599ba0d8f560cbc9
Tim Moore <timmoore226@gmail.com>
2019-04-07 15:27:16 -0400

    Added ratio selector, changed some styling, and adjusted some of the responsiveness for smaller screens.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,81 @@
 // Based on an 8:1 water:coffeebean ratio
-const beanRatio = 1/8;
+let selectRatio = document.querySelector("#select-ratio");
 let waterAmount = document.querySelector("#water-amount");
-let waterMetric = document.querySelector("#water-metric");
+let selectMetric = document.querySelector("#select-metric");
 let resultDisplay = document.querySelector("#result-display");
-let result = document.querySelector("#result");
+
+const ratios = [
+    {
+        label: '1/8',
+        number: .125,
+        description: "Average strength"
+    },
+    {
+        label: '1/4',
+        number: .25,
+        description: "Concentrate; dilute with 1:1 parts water!"
+    },
+    {
+        label: '1/16',
+        number: .0625,
+        description: "Very weak"
+    }
+];
+
+const waterMetric = {
+    'oz': 29.57,
+    'cups': 236.59,
+    'pints': 473.18,
+    'quarts': 946.36,
+    'gallons': 3785
+}
+
+for(index in ratios) {
+    selectRatio.options[selectRatio.options.length] = new Option(ratios[index].label, ratios[index].number);
+}
+
+
+for(index in waterMetric) {
+    selectMetric.options[selectMetric.options.length] = new Option(index, waterMetric[index]);
+}
 
 const makeCoffee = () => {
-    let amount = waterAmount.value;
-    let metric = waterMetric.value;
-    if (metric > 1) {
-        amount = convertToCups(amount, metric);
-    } 
+    validityCheck();
 
-    let coffeeNeeded = amount * beanRatio;
+    const amount = waterAmount.value;
+    const metric = selectMetric.value;
+    const ratio = selectRatio.value;
+    
+    let coffeeNeeded = amount * metric * ratio;
 
-    result.textContent = coffeeNeeded + "oz";
+    // result.textContent = coffeeNeeded + "grams";
+    updateDisplay(coffeeNeeded);
     resultDisplay.style.display = "block";
 }
 
-const convertToCups = (amount, metric) => {
-    return amount * metric;
+const updateDisplay = (amount) => {
+    resetDisplay();
+
+    const resultString = "I recommend about " + Math.round(amount) + "grams (or " + convertMlToOz(amount) + "oz) of coarsely ground coffee beans."
+    var node = document.createElement("text");
+    var textnode = document.createTextNode(resultString);
+    node.appendChild(textnode);
+    resultDisplay.appendChild(node);
+}
+
+const convertMlToOz = (mls) => Number.parseFloat(mls / 28.3495).toPrecision(2);
+
+const resetDisplay = () => {
+    while (resultDisplay.firstChild) {
+        resultDisplay.removeChild(resultDisplay.firstChild);
+    }
+}
+
+const validityCheck = () => {
+    if (waterAmount.validity.badInput) {
+        alert("C'mon now... numbers only, please.");
+    }
+    if (waterAmount.value <= 0) {
+        alert("Amount of water must used must be above 0, obviously.");
+    }
 }
